b448fcd1f4971cf75d753180d64fb4a49cab2f33
kaikkialla <igor.umnyashkin@yandex.ru>
2019-09-24 20:46:26 +0300

    a

---
+++ xor/xor/bubble.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main() {
+	
+	int size;
+	int *array;
+	cin >> size;
+	
+	array = new int[size];
+
+	for (int i = 0; i < size; i++) {
+		cin >> array[i];
+	}
+
+	for (int a = 0; a < size - 1; a++) {
+		for (int b = 0; b < size - a - 1; b++) {
+			if (array[b] > array[b + 1]) {
+				array[b + 1] = array[b + 1] xor array[b];
+				array[b] = array[b + 1] xor array[b];
+				array[b + 1] = array[b + 1] xor array[b];
+			}
+		}
+	}
+
+	return 0;
+}
